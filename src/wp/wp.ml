e29f035fc5d1ee3c2efe8e5cbdad24a11585819e
Simmo Saan <simmo.saan@gmail.com>
2025-05-16 15:53:04 +0300

    Lahenda wp praktikumis

--- src/wp/wp.ml
+++ src/wp/wp.ml
@@ -9,7 +9,19 @@ open Symbolic
     Vihje: Expr.substitute_one.
     Vihje: bool_of_int. *)
 let rec wp (stmt: Ast.stmt) (post: Expr.expr): Expr.expr =
-  failwith "TODO"
+  let open Syntax in
+  match stmt with
+  | Nop -> post
+  | Error -> false_
+  | Assign (x, e) ->
+    let e' = eval_expr e in
+    Expr.substitute_one post !x e'
+  | Seq (s1, s2) -> wp s1 (wp s2 post)
+  | If (c, s1, s2) ->
+    let c' = bool_of_int (eval_expr c) in
+    implies c' (wp s1 post) && implies (not c') (wp s2 post)
+  | While (Num 1, Nop) -> true_
+  | _ -> failwith "TODO"
 
 
 (** Korrektsuse kontrolli tulemus. *)
@@ -25,4 +37,8 @@ type verdict =
     Vihje: match-i Solver.check tulemust.
     Vihje: env_of_model. *)
 let check (stmt: Ast.stmt): verdict =
-  failwith "TODO"
+  let formula = Syntax.(not (wp stmt true_)) in
+  match Solver.check solver [formula] with
+  | SATISFIABLE -> Incorrect (env_of_model (Option.get (Solver.get_model solver)))
+  | UNSATISFIABLE -> Correct
+  | UNKNOWN -> Unknown
